73487b79772df423df34037f265fd9f73352c0d1
Tarun <tarunsharma9117@gmail.com>
2021-07-16 21:04:36 +0530

    commited with comments

--- ex11.py
+++ ex11.py
@@ -1,7 +1,9 @@
-#Email Collector
+# Email Collector/ Fetcher
 
-import  re
-str="""
+import re
+
+# Data consist of many emails
+str = """
 fdknhfklnd hflaskf ksfas sharma@gmail.com khfdslk
 fkjfsz
  kjnjvnfkdj iheoif j bfdkji b elow th eyyenlknklnl asjpovn@gmail.com hdifhidskj hciuashiu@hpoi.in knkdjf hihsdoin
@@ -9,18 +11,8 @@ iufdjankcns this is a text as a s string with emails are tanya2@redcliff.in akhj
 klshsjdh fiku@yahoo.com jkhfhf uihiuefh  start a day with this email kito@gmail.com 
 jjfhs end the day with doip@kopi.com
 """
-
-# check = re.compile(r'\b\s.*@')
-# check = re.compile(r'.*@*\s$')
-# check = re.compile(r'(\S*@*\S)@(\S*@*\S)')
-
-check = re.compile(r'(\S*\S)@(\S*\S)')
+check = re.compile(r'(\S*\S)@(\S*\S)')  #using regular expression for fetching the emails
 matches = check.finditer(str)
+email = re.findall(r'[0-9a-zA-Z._+%]+@[\w]+[.][0-9a-zA-Z]+', str) #equation for collecting emails
+print("Fetched emails are :", email)
 
-# email=re.findall(r'((\S+\S)+@(\S+\S))',str)
-email = re.findall(r'[0-9a-zA-Z._+%]+@[\w]+[.][0-9a-zA-Z]+',str)
-print("This is findall email", email)
-i = 1
-# for match in matches:
-#     print(f"This is Compile Email{i}:{match}")
-#     i += 1
